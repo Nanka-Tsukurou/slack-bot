8ca731637251cae782e5ed79e588a9a8100b297e
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2020-01-03 17:18:14 +0900

    add チャンネル紹介追加

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # slack-bots
 
+## requirements
+
+- pipenv
+- slack botとそのトークン
+
+## usage
+
+### ローカル環境構築
+
+```
+# 環境変数を設定
+$ cp .env.sample .env
+
+# インストール
+$ pipenv install --dev
+
+# 実行
+```
---
+++ src/post_new_channels.py
@@ -0,0 +1,45 @@
+import os
+import datetime
+from logging import getLogger
+from slack import WebClient
+
+logger = getLogger(__name__)
+
+slack_token = os.getenv('SLACK_TOKEN')
+slack_channel = os.getenv('SLACK_CHANNEL')
+jst = datetime.timezone(datetime.timedelta(hours=9))
+
+
+def post_new_channels():
+    threshold = datetime.datetime.now() - datetime.timedelta(hours=0)
+    client = WebClient(slack_token)
+
+    channels = client.channels_list()
+    assert channels['ok']
+
+    target_channels = list(
+        filter(lambda c: c['created'] >= int(threshold.timestamp()),
+               channels['channels']))
+
+    if len(target_channels) == 0:
+        logger.warning('target channels unfound...')
+        return
+
+    users = client.users_list()
+    assert users['ok']
+
+    for channel in target_channels:
+        user = next(
+            filter(lambda m: m['id'] == channel['creator'], users['members']))
+
+        message = '{} が新しいチャンネル {} を {} 頃に作ったみたいや！\n要チェックやでー！！'.format(
+            user['profile']['real_name_normalized'],
+            '<#{}>'.format(channel['id']),
+            datetime.datetime.fromtimestamp(channel['created'],
+                                            jst).strftime('%Y/%m/%d %H:%M'))
+
+        client.chat_postMessage(channel=slack_channel, text=message)
+
+
+if __name__ == "__main__":
+    post_new_channels()
